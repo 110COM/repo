82b943545e0d9773604ad57d815404d58095be76
110COM <136911435+110COM@users.noreply.github.com>
2024-06-10 22:16:01 +0800

    Update hd.py

--- por/hd.py
+++ por/hd.py
@@ -87,7 +87,32 @@ with open(out1_file_path, 'w', encoding='utf-8') as file:
     for name, url in results:
         file.write(f"{name},{url}\n")
         
+
+
+
         
+# 追加数据
+
+try:
+    # 打开并读取参考.txt 文件，编码为utf8
+    with open("jiee/local", 'r', encoding='utf-8') as reference_file:
+        reference_data = reference_file.read()
+
+    # 把读取的数据写入到原始.txt 文件末尾
+    with open("jiee/hd", 'a', encoding='utf-8') as original_file:
+        original_file.write('\n')
+        original_file.write(reference_data)
+
+    # 完成后，打印消息
+    print("已完成追加数据！")
+
+except FileNotFoundError as e:
+    print(f"文件未找到：{e.filename}")
+except Exception as e:
+    print(f"发生错误：{e}")
+
+
+
         
 
 cctv_channels = []
@@ -103,6 +128,11 @@ henan_channels = []
 shandong_channels = []
 fujian_channels = []
 guangxi_channels = []
+
+anhui_channels = []
+jiangsu_channels = []
+sichuan_channels = []
+
 other_channels2 = []
 
 
@@ -135,17 +165,21 @@ with open(in1_file_path, 'r',  encoding='utf-8') as file:
 
         channel_keywords = {
             'cctv_channels': ['CCTV'],
-            'sport_channels': ['CCTV5', '体育', '足球'],
+            'sport_channels': ['CCTV5', '体育', '足球', '篮球', '羽毛球'],
             'child_channels': ['CCTV14', '卡通', '少儿', '哈哈炫动', '动漫', '动画'],
             'satellite_channels': ['卫视'],
-            'guangdong_channels': ['广东', '广州', '惠州', '河源', '东莞', '梅州', '深圳', '潮州', '珠江', '揭西', '佛山'],
-            'hunan_channels': ['湖南', '金鹰', '茶'],
+            'guangdong_channels': ['广东', '广州', '佛山', '惠州', '河源', '东莞', '梅州', '深圳', '潮州', '珠江', '揭西', '汕头', '汕尾', '清远', '湛江', '珠海', '肇庆', '茂名', '韶关', '客家', '中山', '云浮', '揭阳', '徐闻', '番禺', '徐闻', '大浦'],
+            'hunan_channels': ['湖南', '金鹰', '茶', '娄底', '常德', '张家界', '永州', '浏阳', '湘西', '衡阳', '邵阳', '长沙', '桂东', '桂阳'],
             'zhejiang_channels': ['浙江', '杭州', '西湖明珠', '宁波', '上虞', '丽水', '松阳', '永嘉', '温州', '绍兴', '苍南', '衢州', '诸暨', '遂昌', '青田', '龙泉', '钱江'],
-            'hebei_channels': ['河北', '保定', '张家口', '涿州', '石家庄', '邯郸', '魏县', '定州', '安新', '定兴', '涞源'],
+            'hebei_channels': ['河北', '保定', '张家口', '涿州', '石家庄', '邯郸', '魏县', '定州', '安新', '定兴', '涞源', '兴隆', '大厂', '清河', '任丘', '昌黎', '沧州', '鹿泉', '平泉', '徐水'],
             'henan_channels': ['河南', '南阳', '郑州'],
-            'shandong_channels': ['山东', '济南', '济宁', '胶州'],
-            'fujian_channels': ['福建'],
-            'guangxi_channels': ['广西', '南宁']
+            'shandong_channels': ['山东', '济南', '济宁', '胶州', '青州', '烟台', '枣庄', '菏泽', '威海', '临沂'],
+            'fujian_channels': ['福建', '厦门', '新罗', '云霄', '建宁', '漳州', '三明'],
+            'guangxi_channels': ['广西', '南宁', '玉林', '防城港', '梧州', '钦州', '柳州', '来宾', '贺州', '河池', '桂林', '贵港', '北海', '百色', '罗城', '凌云', '凤山', '天等', '崇左'],
+			
+			'anhui_channels': ["安徽", "临泉", "义安", "亳州", "六安", "南陵", "合肥", "太湖", "徽州", "寿县", "桐城", "池州", "泗县", "淮北", "淮南", "湾沚", "滁州", "祁门", "繁昌", "蒙城", "阜南", "阜阳", "霍山", "霍邱", "黄山", "岳西", "固镇", "宿州", "肥西", "宿州", "宿州"],
+			'jiangsu_channels': ['江苏', '连云港', '江宁', '海安', '泗阳', '镇江', '江阴', '苏州', '南京', '扬州'],
+			'sichuan_channels': ['四川','乐山', '内江', '凉山', '南充', '宜宾', '巴中', '广元', '广安', '德阳', '眉山', '绵阳', '自贡', '资阳', '遂宁', '阿坝', '熊猫', '汽摩', '蓉城', '成都', '乐至', '什邡', '汶川', '泸县', '泸州', '洪雅', '达州', '黑水', '渑池', '万源', '松潘', '西青', '金川', '长宁', '马尔', '利州', '康定', '甘孜', '营山', '青神'],
         
         }
 
@@ -208,6 +242,10 @@ shandong_channels_sorted = group_and_sort_channels(shandong_channels)
 fujian_channels_sorted = group_and_sort_channels(fujian_channels)
 guangxi_channels_sorted = group_and_sort_channels(guangxi_channels)
 
+anhui_channels_sorted = group_and_sort_channels(anhui_channels)
+jiangsu_channels_sorted = group_and_sort_channels(jiangsu_channels)
+sichuan_channels_sorted = group_and_sort_channels(sichuan_channels)
+
 other_channels2_sorted = group_and_sort_channels(other_channels2)
 
 
@@ -241,13 +279,20 @@ channel_lists = {
     "少儿": child_channels_sorted,
     "其它": other_channels_sorted,
     "广东":guangdong_channels_sorted,
+	"安徽":anhui_channels_sorted,
     "湖南":hunan_channels_sorted,
+	"江苏":jiangsu_channels_sorted,
     "浙江":zhejiang_channels_sorted,
     "河北":hebei_channels_sorted,
     "河南":henan_channels_sorted,
     "山东":shandong_channels_sorted,
     "福建":fujian_channels_sorted,
     "广西":guangxi_channels_sorted,
+	"四川":sichuan_channels_sorted,
+	
+	
+	
+	
 }
 
 
